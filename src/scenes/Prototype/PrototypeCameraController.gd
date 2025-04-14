b70fcd29750f2a2a21e27d4bb43a6ca7034fa2d4
Tibor <tiborcmester@gmail.com>
2025-04-13 23:19:42 -0400

    changed the player dampening to be aligned with the rope instead of the other player

--- src/scenes/Prototype/PrototypeCameraController.gd
+++ src/scenes/Prototype/PrototypeCameraController.gd
@@ -3,9 +3,12 @@ extends Camera2D
 @export var cargo_scene : PackedScene
 @export var left_character : Character
 @export var right_character : Character
-@export var ropes : Array[Rope] = []
+@export var left_rope : Rope
+@export var right_rope : Rope
+
 @export var strength: float = 400.0
-@export var max_distance : float = 0
+
+@export var player_leash_multiplier : float = 1.1
 
 @export var player_dampening : float = 2
 
@@ -15,29 +18,19 @@ func _process(delta):
 		get_parent().add_child(cargo)
 		cargo.global_position = get_global_mouse_position()
 
-func _ready() -> void:
-	if max_distance == 0: max_distance = get_max_distance()
-		
-
-func get_max_distance() -> float:
-	var attachements : Array[Vector2] = []
-	var distance : float = 0
-	for rope in ropes:
-		distance += rope.spring_length
-		attachements.append(rope.platform_attachement.global_position)
-	distance += (attachements[0] - attachements[1]).length()
-	return distance
-
 
 func _physics_process(delta: float) -> void:
 	for suffix in ["L", "R"]:
 		var character : Character = right_character
+		var rope : Rope = right_rope
 		var force : Vector2 = get_input_vector(suffix) * strength
-		var difference : Vector2 = right_character.global_position - left_character.global_position
 		if suffix == "L": 
-			difference *= -1
 			character = left_character
-		if difference.length() > max_distance:
+			rope = left_rope
+
+		var difference : Vector2 = character.global_position - rope.platform_attachement.global_position
+		
+		if difference.length() > rope.spring_length * player_leash_multiplier:
 			#subtract the force component that is in the away direction
 			var direction : Vector2 = difference.normalized()
 			force = force - (max(force.dot(direction),0) * direction)
