61c8af68c379507c027fc7f67bfa5b8def8f4705
MossWyrm <r.j.morecraft@gmail.com>
2025-04-14 17:41:38 +0100

    Collection zone, drop mechanic & renaming
    renamed scripts/scenes to match conventions

--- src/scenes/Prototype/obstacle.gd
+++ src/scenes/Prototype/obstacle.gd
@@ -6,8 +6,9 @@ extends Path2D
 ##obstacle movement speed
 @export var speed: float
 
-@onready var path_follow: PathFollow2D = get_node("PathFollow2D")
-@onready var sprite: Sprite2D = get_node("Sprite2D")
+@export_group("References")
+@export var sprite: Sprite2D
+@export var path_follow: PathFollow2D
 
 var _invert_direction: bool = false
 
@@ -19,10 +20,10 @@ func _physics_process(delta: float) -> void:
 	
 func _move_obstacle(delta: float) -> void:
 	if _invert_direction:
-		path_follow.progress_ratio -= (delta * speed)
+		path_follow.progress -= (delta * speed)
 		if path_follow.get_progress_ratio() <= 0:
 			_invert_direction = false
 	else:
-		path_follow.progress_ratio += (delta*speed)
+		path_follow.progress += (delta*speed)
 		if path_follow.get_progress_ratio() >= 1:
 			_invert_direction = true
