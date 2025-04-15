fc1043a6532616625ea47ea6f06b957979e21273
Tibor <tiborcmester@gmail.com>
2025-04-15 18:36:35 -0400

    fixed the merge (deleted prototype)

--- src/scenes/Prototype/Character.gd
+++
@@ -1,8 +0,0 @@
-class_name Character extends RigidBody2D
-
-func _physics_process(delta: float) -> void:
-	apply_torque(get_orientation_torque())
-
-@export var orientation_strength : float = 1500
-func get_orientation_torque() -> float:
-	return (0 - rotation) * orientation_strength
