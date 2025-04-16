adc997c3eb2b84c17bc3097274d4be76bfbf5744
Tibor <tiborcmester@gmail.com>
2025-04-15 23:34:13 -0400

    Added Boundary Walls for the Player + (Refactored Parachute Walls)

---
+++ src/scenes/Game/Components/Walls/BoundaryWalls.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var phantom_camera : PhantomCamera2D
+@onready var _floor : StaticBody2D = get_node("Floor")
+@onready var _ceiling : StaticBody2D = get_node("Ceiling")
+@onready var _left_wall : StaticBody2D = get_node("LeftWall")
+@onready var _right_wall : StaticBody2D = get_node("RightWall")
+
+##how many extra pixels the camera gets too see beyond the boundary walls
+@export var border : int = 25
+
+func _ready() -> void:
+	phantom_camera.limit_bottom = floor(_floor.global_position.y) + border
+	phantom_camera.limit_right = floor(_right_wall.global_position.x) + border
+	phantom_camera.limit_top = ceil(_ceiling.global_position.y) - border
+	phantom_camera.limit_left = ceil(_left_wall.global_position.x) - border
---
+++ src/scenes/Game/Components/Walls/RespawnAreas.gd
@@ -0,0 +1,39 @@
+##Respawns dropped cargo and spawns new ones around the platform
+class_name RespawnAreas extends Node2D
+
+##Will spawn cargo above this node
+@export var platform : Node2D
+@onready var _ceiling : Area2D = get_node("Ceiling")
+@onready var _timer : Timer = get_node("Timer")
+
+var cargo_scene : PackedScene = preload("res://src/scenes/Game/Components/Cargo/Cargo.tscn")
+
+##Maximum time for a box to spawn
+@export var max_spawn_frequency : float = 7
+##Minimum time for a box to spawn
+@export var min_spawn_frequency: float = 3
+func _onTimerTimeout():
+	_timer.start(randf_range(min_spawn_frequency, max_spawn_frequency))
+	SpawnParachuteCrates()
+
+
+##Connected in editor to the body_entered signal of the floor
+func _onFloorBodyEntered(cargo : Node2D):
+	if cargo is Cargo and cargo.respawn:
+		##Centered on the platform is likely more ideal
+		#var pos : Vector2 = Vector2(body.global_position.x, _ceiling.global_position.y)
+		var pos : Vector2 = Vector2(platform.global_position.x, _ceiling.global_position.y)
+		cargo.queueTeleport(pos, Vector2.ZERO, 0, 0, true)
+
+
+##How far away from the platform can these boxes spawn
+@export var random_cargo_spawn_range : float = 200
+
+func SpawnParachuteCrates():
+	var cargo : Cargo = cargo_scene.instantiate()
+	cargo.respawn = false
+	cargo.global_position = Vector2(platform.global_position.x 
+		+ randf_range(-random_cargo_spawn_range, random_cargo_spawn_range),
+		_ceiling.global_position.y)
+	get_parent().add_child(cargo)
+	cargo.setParachute(true)
