492af062cf4da9850757763df1aefd3abd154381
Tibor <tiborcmester@gmail.com>
2025-04-15 22:34:28 -0400

    Added a random cargo spawner and fixed parachutes persisting for landed cargo

--- src/scenes/Game/Components/Cargo/Cargo.gd
+++ src/scenes/Game/Components/Cargo/Cargo.gd
@@ -8,8 +8,7 @@ class_name Cargo extends RigidBody2D
 @onready var r_string: Line2D = get_node("Parachute String R")
 @onready var parachute_sprite: Sprite2D = get_node("ParachuteSprite")
 
-
-
+@export var respawn : bool = true
 
 func destroy() -> void:
 	sprite.visible = false
@@ -19,48 +18,67 @@ func destroy() -> void:
 
 
 ##Called by the box recovery mechanic to teleport the cargo back to the top
-func queueTeleport(pos : Vector2 = global_position, vel : Vector2 = linear_velocity, rot : float = global_rotation, angular_vel : float = angular_velocity, _parachute : bool = false):
-	queued = true
-	queued_pos = pos
-	queued_rot = rot
-	queued_vel = vel
-	queued_angular_vel = angular_vel
-	if _parachute:
-		_setParachuteVisibility(true)
-		self.parachute = true
-		self.gravity_scale = 0.5
+func queueTeleport(pos : Vector2 = global_position, vel : Vector2 = linear_velocity, rot : float = global_rotation, angular_vel : float = angular_velocity, parachute : bool = false):
+	_queued = true
+	_queued_pos = pos
+	_queued_rot = rot
+	_queued_vel = vel
+	_queued_angular_vel = angular_vel
+	setParachute(parachute)
+	
+##Makes the parachute visible and slows the cargo's speed
+func setParachute(enabled : bool = true):
+	_setParachuteVisibility(enabled)
+	_parachute = enabled
+	self.gravity_scale = 0.5 if enabled else 1.0
 
 ##Need to store these until the game loop calls integrate forces
-var queued : bool = false
-var queued_pos : Vector2
-var queued_rot : float
-var queued_vel : Vector2
-var queued_angular_vel : float
-var parachute : bool = false
+var _queued : bool = false
+var _queued_pos : Vector2
+var _queued_rot : float
+var _queued_vel : Vector2
+var _queued_angular_vel : float
+var _parachute : bool = false
 @export var parachute_speed_limit : float = 35.0
 ##changes the body in the physics simulation, hopefully the rest of the node goes with it?
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	if queued:
-		state.transform = Transform2D(queued_rot, queued_pos)
-		state.linear_velocity = queued_vel
-		state.angular_velocity = queued_angular_vel
-		queued = false
-	if parachute:
+	if _queued:
+		state.transform = Transform2D(_queued_rot, _queued_pos)
+		state.linear_velocity = _queued_vel
+		state.angular_velocity = _queued_angular_vel
+		_queued = false
+	if _parachute:
 		state.linear_velocity = state.linear_velocity.limit_length(parachute_speed_limit)
 
 func _onBodyEntered(body : Node2D):
 	##exclude hitting other cargo
-	if parachute and ((body is RigidBody2D and not body is Cargo) or body is TileMap) :
+	if _parachute and ((body is RigidBody2D and not body is Cargo) or body is TileMap) :
 		self.gravity_scale = 1.0
 		_setParachuteVisibility(false)
-		parachute = false
+		_parachute = false
+
+##How long a cargo needs to be stationary before the parachute dissapearing
+@export var at_rest_duration : float = 2
+var _at_rest_counter : float = 0
 
 func _physics_process(delta: float) -> void:
-	if parachute:
+	if _parachute:
+		_checkAtRest(delta)
 		##TODO fix this Simulate some wind every now and then
 		#if (randi() % 250 == 0): _apply_random_force()
 		_updateParachuteAnimation()
+
 		
+
+##turns the parachute off if we are stationary
+func _checkAtRest(delta : float):
+	if linear_velocity.length_squared() < 0.25:
+		_at_rest_counter += delta
+		if _at_rest_counter > at_rest_duration:
+			setParachute(false)
+			_at_rest_counter = 0
+	else: _at_rest_counter = 0
+	
 			
 			
 ##applies a random force at a random position to simulate a bit of wind, its kind of awful tho
--- src/scenes/Game/Components/Parachute/Ceiling.gd
+++ src/scenes/Game/Components/Parachute/Ceiling.gd
@@ -1,12 +1,38 @@
-extends Area2D
+class_name Ceiling extends Area2D
 
 @onready var platform : RigidBody2D = get_node("../PlayerCharacter/Platform")
-
+var cargo_scene : PackedScene = preload("res://src/scenes/Game/Components/Cargo/Cargo.tscn")
 
 ##Connected in editor to the body_entered signal of the floor
-func _onFloorBodyEntered(body : Node2D):
-	if body is Cargo:
+func _onFloorBodyEntered(cargo : Node2D):
+	if cargo is Cargo and cargo.respawn:
 		##Centered on the platform is likely more ideal
 		#var pos : Vector2 = Vector2(body.global_position.x, self.global_position.y)
 		var pos : Vector2 = Vector2(platform.global_position.x, self.global_position.y)
-		body.queueTeleport(pos, Vector2.ZERO, 0, 0, true)
+		cargo.queueTeleport(pos, Vector2.ZERO, 0, 0, true)
+
+
+
+func _ready():
+	var timer : Timer = Timer.new()
+	add_child(timer)
+	timer.timeout.connect( func ():
+		timer.start(randf_range(min_spawn_frequency, max_spawn_frequency ))
+		SpawnParachuteCrates())
+	timer.start((min_spawn_frequency + max_spawn_frequency)/ 2)
+
+##Maximum time for a box to spawn
+@export var max_spawn_frequency : float = 7
+##Minimum time for a box to spawn
+@export var min_spawn_frequency: float = 3
+##How far away from the platform can these boxes spawn
+@export var random_cargo_spawn_range : float = 200
+
+func SpawnParachuteCrates():
+	var cargo : Cargo = cargo_scene.instantiate()
+	cargo.respawn = false
+	cargo.global_position = Vector2(platform.global_position.x 
+		+ randf_range(-random_cargo_spawn_range, random_cargo_spawn_range),
+		self.global_position.y)
+	add_child(cargo)
+	cargo.setParachute(true)
