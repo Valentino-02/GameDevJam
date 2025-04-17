facf2150cf7e4d2ecfbff133d5cd93c348a7e71d
Tibor <tiborcmester@gmail.com>
2025-04-16 23:42:08 -0400

    Fireball Finished
    (Explosion particles need work)

---
+++ src/scenes/Game/Components/Fireball/Fireball.gd
@@ -0,0 +1,44 @@
+class_name Fireball extends RigidBody2D
+##a multiplier to the explosion force
+@export var explosion_strength : float = 1.0
+##(radius-Distance)^power = strength coefficient, 0 = constant, 1 = linear, 2 = quadratic, etc...
+@export var strength_fall_off_power : int = 0
+
+@onready var _explosion_emitter : GPUParticles2D = get_node("GPUParticles2D")
+@onready var _fireball_sprite : Sprite2D = get_node("FireballSprite")
+@onready var _shape_cast : ShapeCast2D = get_node("ShapeCast2D")
+@onready var _ray_cast : RayCast2D = get_node("RayCast2D")
+const STRENGTH_MULTIPLIER : float = 200
+
+
+##Pushes everything with los to the collision away
+func _onCollision(_collided : Node) -> void:
+	_explosion_emitter.emitting = true
+	_fireball_sprite.visible = false
+	var bodies : Array = getBodiesInRadius()
+	for body in bodies:
+		if not body is RigidBody2D or body == self: continue
+		if getRayCast(body) == body:
+			var diff : Vector2 = _ray_cast.get_collision_point() - self.global_position
+			var explosion_radius : float = _shape_cast.shape.radius
+			var force : float = pow(clampf((explosion_radius - diff.length()), 0, explosion_radius)/explosion_radius, strength_fall_off_power) * STRENGTH_MULTIPLIER * explosion_strength
+			var direction : Vector2 = diff.normalized()
+			body.apply_impulse(direction * force * body.mass, _ray_cast.get_collision_point())
+
+	await get_tree().create_timer(0.4).timeout
+	self.queue_free()
+
+##Finds the first body on the path there
+func getRayCast(body : RigidBody2D):
+	_ray_cast.target_position = _ray_cast.to_local(body.global_position)
+	_ray_cast.force_raycast_update()
+	return _ray_cast.get_collider()
+
+##Detects anybody within the collision radius
+func getBodiesInRadius() -> Array:
+	_shape_cast.force_shapecast_update()
+	var collision = []
+	collision.resize(_shape_cast.get_collision_count())
+	for i in range(_shape_cast.get_collision_count()):
+		collision[i] = _shape_cast.get_collider(i)
+	return collision
