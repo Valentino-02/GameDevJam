be4b72ad92bca06acaf00043599f236a7816dff1
tibormester <67936278+tibormester@users.noreply.github.com>
2025-04-18 13:52:12 -0400

    added particle effects to characters. Disabled in editor and launch when ready. (#24)
    Also added plain particle for use anywhere
    
    Co-authored-by: MossWyrm <r.j.morecraft@gmail.com>

--- src/scenes/Game/PlayerCharacter/Character.gd
+++ src/scenes/Game/PlayerCharacter/Character.gd
@@ -2,7 +2,10 @@ class_name Character extends RigidBody2D
 
 @export var orientationStrength : float = 1500
 
-
+func _ready() -> void:
+	var _particles: GPUParticles2D = $GPUParticles2D
+	_particles.emitting = true
+	
 func _physics_process(_delta: float) -> void:
 	apply_torque(_getOrientationTorque())
 
