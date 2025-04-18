eee7146e95157e8c8581711f362c9adee7207ae4
tibormester <67936278+tibormester@users.noreply.github.com>
2025-04-18 14:46:19 -0400

    Integration2 (#28)
    * minor pull request bug fixes
    
    * Animated rope (#23)
    
    * added animation to ropes using Particle Effects
    
    * added animation to the dropping mechanic, including splitting sprite
    
    ---------
    
    Co-authored-by: MossWyrm <r.j.morecraft@gmail.com>
    
    * fixed corrupted tscn
    
    * added particle effects to characters. Disabled in editor and launch when ready. (#24)
    
    Also added plain particle for use anywhere
    
    Co-authored-by: MossWyrm <r.j.morecraft@gmail.com>
    
    * fixed game secene formatting
    
    * added ability to add points of interest to the map (#25)
    
    Co-authored-by: MossWyrm <r.j.morecraft@gmail.com>
    
    * Character physics (#26)
    
    * Character Physics Improvement
    
    * fixed an issue where gravity was too strong when going down at an angle that has a dot product greater than 80%
    
    * fixed some of the stuttering when at the end of the leash by linearly easing into the velocity dampening
    
    also potentially fixed the bug where explosions would fail to accelerate the player beyond their max speed
    
    * fixed shared rope particle mask bug
    
    * Named and Added CollisionLayers (#27)
    
    layer_1 = "Terrain"
    layer_2 = "BluePlayer"
    layer_3 = "RedPlayer"
    layer_4 = "Platform"
    layer_5 = "Cargo"
    
    Co-authored-by: MossWyrm <r.j.morecraft@gmail.com>
    
    * updated the fireball and raincloud's collision masks
    
    ---------
    
    Co-authored-by: MossWyrm <r.j.morecraft@gmail.com>

--- src/scenes/Game/PlayerCharacter/Rope.gd
+++ src/scenes/Game/PlayerCharacter/Rope.gd
@@ -11,18 +11,16 @@ class_name Rope extends Line2D
 ##This is to prevent the platform overshooting due to the iterative nature of game physics. An unintended (beneficial?) consequence is that it acts as a weight limit
 @export var maxForce : float = 10000
 @export var maxStretchPercent : float = 0.1
-@export var playerCharacter : PlayerCharacter
 @export var character: Character
 @export var platform: RigidBody2D
 @export var platform_attachement : Node2D
-
 
 func _physics_process(_delta):
 	var a_pos = character.global_position
 	var b_pos = platform_attachement.global_position
 
 	# Update line points
-	points = [a_pos - playerCharacter.position, b_pos - playerCharacter.position]
+	points = [to_local(a_pos), to_local(b_pos)]
 
 	# Spring vector
 	var diff = b_pos - a_pos
@@ -45,14 +43,14 @@ func _physics_process(_delta):
 	var mv : float = _calculateCriticalDampening(platform.mass, platformSpringStrength) * platform_vel
 	var platform_force : Vector2 = -direction * (kx - mv)
 	platform_force = platform_force.limit_length(maxForce * platform.mass)
-	
+	# calculate for player
 	kx = playerSpringStrength * displacement
-	mv  = _calculateCriticalDampening(character.mass, playerSpringStrength) * player_vel
-	var player_force : Vector2 = direction * (kx)# - mv)
+	#mv  = _calculateCriticalDampening(character.mass, playerSpringStrength) * player_vel
+	var player_force : Vector2 = direction * kx #- mv)
 	player_force = player_force.limit_length(maxForce * character.mass)
 	#Reduce gravity only if we are close
 	if displacement < springLength * maxStretchPercent:
-		player_force += (-1 + playerGravityCoefficient) * PlayerCharacter.getComponentAlongDirection(player_force, direction)
+		player_force += (-1 + playerGravityCoefficient) * Character.getComponentAlongDirection(player_force, direction)
 	
 	character.apply_force(player_force)
 	platform.apply_force(platform_force, platform_attachement.global_position - platform.global_position)
