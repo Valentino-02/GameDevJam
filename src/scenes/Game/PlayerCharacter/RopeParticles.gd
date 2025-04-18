b038c3b44c720bb5a79910dd7c0b55ce5029db40
Tibor <tiborcmester@gmail.com>
2025-04-18 14:35:23 -0400

    fixed shared rope particle mask bug

--- src/scenes/Game/PlayerCharacter/RopeParticles.gd
+++ src/scenes/Game/PlayerCharacter/RopeParticles.gd
@@ -10,6 +10,3 @@ func _physics_process(_delta: float) -> void:
 	_mask.position = _rope.points[0]
 	_mask.rotation = _rope.points[0].direction_to(_rope.points[1]).angle()
 	_mask.texture.size.x = _rope.points[0].distance_to(_rope.points[1])
-	
-
-	
