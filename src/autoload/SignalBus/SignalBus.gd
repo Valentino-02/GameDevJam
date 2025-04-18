ebbcc6b54e5bc492b58b5b2c45df83957a9f2026
MossWyrm <74151180+MossWyrm@users.noreply.github.com>
2025-04-18 18:33:51 +0100

    added fire & ice platforms with simple particles. Extinguishable with correct cargo (#19)

--- src/autoload/SignalBus/SignalBus.gd
+++ src/autoload/SignalBus/SignalBus.gd
@@ -3,5 +3,7 @@ extends Node
 @warning_ignore_start("unused_signal")
 signal zoneGotCargo(zone: Types.Zone)
 signal zonePatienceChanged(zone: Types.Zone, value: float)
+signal cargoDroppedOnSurface(hitPoint: Vector2, cargo: Cargo)
+signal hazardFixed(element: Types.Element)
 signal restart
 signal game_over
