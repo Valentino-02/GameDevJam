4c196c17127f96d8d74c39722f0f65a6979ba53e
valentino <collazo.valentino@gmail.com>
2025-04-19 16:24:20 -0300

    add timer paramaters

--- src/scenes/Game/Levels/Level.gd
+++ src/scenes/Game/Levels/Level.gd
@@ -1,6 +1,8 @@
 class_name Level extends Node2D
 
 @export var maxScore : float = 15.0
+@export var patienceLossPerSecond : float = 1.0
+@export var cargoPatienceGain : float = 10.0
 
 @onready var camera : Camera2D = %Camera2D
 @onready var tileMapLayer : TileMapLayer = %TileMapLayer
