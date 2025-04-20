74a5565ec26f899a68796b10455b48d7c33c4abe
valentino <collazo.valentino@gmail.com>
2025-04-20 11:40:53 -0300

    add juice to the score

--- src/scenes/Game/Components/Cargo/FleetingNumber.gd
+++ src/scenes/Game/Components/Cargo/FleetingNumber.gd
@@ -1,7 +1,5 @@
 class_name FleetingNumber extends Node2D
 
-@onready var _sprite : Sprite2D = $Sprite2D
-
 func _ready():
 	var tween := create_tween()
 	tween.set_parallel(true)
@@ -12,3 +10,4 @@ func _ready():
 	modulate.a = 1.0
 	tween.tween_property(self, "modulate:a", 0.0, 0.6).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	await tween.finished
+	queue_free()
