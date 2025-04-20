5639cc7f69006c1eca30c71c7d9b2d4c0f2507d2
tibormester <67936278+tibormester@users.noreply.github.com>
2025-04-20 04:12:31 -0400

    Fake joystick controls (#36)
    * FAKE JOYSTICK CONTROLS
    
    * made the platform spring less stiff
    
    * shrunk packages
    
    * cargo tiny

--- src/scenes/Game/PlayerCharacter/Character.gd
+++ src/scenes/Game/PlayerCharacter/Character.gd
@@ -1,62 +1,51 @@
-class_name Character extends RigidBody2D
+class_name Character extends Node2D
 
 
 @export var orientationStrength: float = 5000
-@export var maxFlySpeed: float = 1000
-@export var minFlySpeed: float = 250
-@export var flyingAcceleration: float = 400
-##How much the player can stretch the rope before inputs get ignored
-@export var _leashLength: float = 1.2
-##'L' or 'R' so we can detect inputs
+
 @export var _suffix: String = "L"
 @export var _rope: Rope
 @export var element: Types.Element
 
+@export var _radius : float = 50
+@export var _speed : float = 200
+@export var returnSpeed : float = 200
+var outputDir : Vector2
 
-var linearVelocity: Vector2 = Vector2.ZERO
+@onready var _origin : Marker2D = get_node("../"+_suffix + "Origin")
 
 func _ready() -> void:
 	var _particles: GPUParticles2D = $GPUParticles2D
 	_particles.emitting = true
+	#_rope.springLength = _origin.global_position.distance_to(_rope.platform_attachement.global_position)
+	_origin.global_position = _rope.platform_attachement.global_position + Vector2.UP * _rope.springLength
 	
 
-func _getOrientationTorque() -> float:
-	return (0 - rotation) * orientationStrength
 
 
-#Give instant vel
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	var difference: Vector2 = global_position - _rope.platform_attachement.global_position
-	var direction: Vector2 = difference.normalized()
-	var velocity: Vector2
-	var input_dir: Vector2 = _getInputVector()
+#get the target position = back to origin or current position + speed in input direciton
+func _physics_process(delta: float) -> void:
+	var inputDir : Vector2 = _getInputVector()
+	_origin.global_position = _rope.platform_attachement.global_position + Vector2.UP * _rope.springLength
+	var origin : Vector2 = _origin.global_position
 
-	#accelerate the current speed
-	var speed: float = linear_velocity.dot(input_dir)
-	var accel: float = get_physics_process_delta_time() * flyingAcceleration
-	#if we are going down enable gravity
-	var grav_mag := input_dir.dot(Vector2.DOWN)
-	if grav_mag >= 0.8: # if trying to fall
-		#normal acceleration
-		velocity = input_dir * (speed + accel)
-		#gravity acceleration
-		velocity += Vector2.DOWN * (get_physics_process_delta_time() * ProjectSettings.get_setting("physics/2d/default_gravity") * grav_mag)
-	else: # any other powered movement that isn't down
-		#accelerates within the bounds, or lets it go wild
-		speed = max(speed, clampf(speed + accel, minFlySpeed, maxFlySpeed))
-		speed = clampf(speed, minFlySpeed, maxFlySpeed)
-		velocity = input_dir * speed
-	#check if we are leaving the rope's range, if so remove the component that is doing so
-	var extra: float = (difference.length() - _rope.springLength) / _rope.springLength
-	if extra > 0.0:
-		var factor: float = clampf((1 - ((_leashLength - extra) / _leashLength)), 0, 1)
-		velocity -= factor * getComponentAlongDirection(velocity, direction)
-	#Check if we are under user input, if so, overwrite the velocity to the input velocity
-	if velocity.length_squared() > 1.0:
-		state.linear_velocity = velocity
+	#move towards the joystick's origin if no input
+	if inputDir == Vector2.ZERO:
+		var distance : float = global_position.distance_to(origin)
+		global_position = global_position.lerp(origin, clampf((returnSpeed * delta)/distance, 0, 1))
 
-	apply_torque(_getOrientationTorque())
+	else:
+		global_position += _speed * delta * inputDir
+		#global_position = global_position.lerp(target, ())
 
+	#If beyond the radius, move back to within
+	var difference : Vector2 = (global_position - origin)
+	global_position = origin + difference.limit_length(_radius)
+	
+	outputDir = global_position - origin
+
+
+	
 func _getInputVector() -> Vector2:
 	var input_vector: Vector2 = Vector2.ZERO
 	if Input.is_action_pressed("Right " + _suffix):
--- src/scenes/Game/PlayerCharacter/Rope.gd
+++ src/scenes/Game/PlayerCharacter/Rope.gd
@@ -4,13 +4,7 @@ class_name Rope extends Line2D
 @export var springLength: float = 150.0
 ##How hard the string pulls the platform towards the player
 @export var platformSpringStrength: float = 5000
-##Player Spring Strength
-@export var playerSpringStrength : float = 600
-##How much of the platform's gravity is transfered to the player
-@export var playerGravityCoefficient : float = 0.1
-##This is to prevent the platform overshooting due to the iterative nature of game physics. An unintended (beneficial?) consequence is that it acts as a weight limit
 @export var maxForce : float = 10000
-@export var maxStretchPercent : float = 0.1
 @export var character: Character
 @export var platform: RigidBody2D
 @export var platform_attachement : Node2D
@@ -32,8 +26,6 @@ func _physics_process(_delta):
 	if displacement < 0:
 		return
 
-	# Relative velocity
-	var _player_vel = character.linear_velocity.dot(direction)
 	var platform_vel = platform.linear_velocity.dot(-direction)
 
 	#Spring force = kx (spring constant * displacement) - mv (mass * velocity)
@@ -43,16 +35,7 @@ func _physics_process(_delta):
 	var mv : float = _calculateCriticalDampening(platform.mass, platformSpringStrength) * platform_vel
 	var platform_force : Vector2 = -direction * (kx - mv)
 	platform_force = platform_force.limit_length(maxForce * platform.mass)
-	# calculate for player
-	kx = playerSpringStrength * displacement
-	#mv  = _calculateCriticalDampening(character.mass, playerSpringStrength) * player_vel
-	var player_force : Vector2 = direction * kx #- mv)
-	player_force = player_force.limit_length(maxForce * character.mass)
-	#Reduce gravity only if we are close
-	if displacement < springLength * maxStretchPercent:
-		player_force += (-1 + playerGravityCoefficient) * Character.getComponentAlongDirection(player_force, direction)
-	
-	character.apply_force(player_force)
+
 	platform.apply_force(platform_force, platform_attachement.global_position - platform.global_position)
 
 
