78d767074a895241e73089fcc12b8b98df635668
MossWyrm <r.j.morecraft@gmail.com>
2025-04-22 20:49:39 +0100

    finished hazard level tutorial to a simple degree

--- src/scenes/Game/Components/Walls/WindObstacle.gd
+++ src/scenes/Game/Components/Walls/WindObstacle.gd
@@ -54,11 +54,11 @@ func Activate(_node: Node2D = null) -> void:
 	_changeParticleSpeed(500)
 	_changeParticleQuantity(12)
 	_audioPlayer.play(0.0)
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3,true).timeout
 	_active = true
-	await get_tree().create_timer(blastDuration).timeout
+	await get_tree().create_timer(blastDuration,true).timeout
 	_active = false
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3,true).timeout
 	_audioPlayer.stop()
 	_changeParticleSpeed(80)
 	_changeParticleQuantity(8)
