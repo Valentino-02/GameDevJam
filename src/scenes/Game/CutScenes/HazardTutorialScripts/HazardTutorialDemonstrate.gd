a8365381f162ac2ff5dee8347d8411f0b7f1ead7
MossWyrm <r.j.morecraft@gmail.com>
2025-04-22 20:31:36 +0100

    finished hazard level tutorial to a simple degree

--- src/scenes/Game/CutScenes/HazardTutorialScripts/HazardTutorialDemonstrate.gd
+++ src/scenes/Game/CutScenes/HazardTutorialScripts/HazardTutorialDemonstrate.gd
@@ -5,6 +5,8 @@ extends CutsceneBehaviour
 func Activate() -> void:
 	for hazard in hazardsToDemonstrate:
 		hazard.process_mode = Node.PROCESS_MODE_ALWAYS
+		if hazard is WindObstacle:
+			hazard.Activate()
 	
 	
 func Finish() -> void:	
