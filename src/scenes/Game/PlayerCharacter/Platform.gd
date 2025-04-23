57d21c8f84da8a90a53d351abb285ea114142509
Tibor <tiborcmester@gmail.com>
2025-04-23 18:09:04 -0400

    changed the cargo dropping to be more responsive, it might be slightly easier to pickup cargo but not really

--- src/scenes/Game/PlayerCharacter/Platform.gd
+++ src/scenes/Game/PlayerCharacter/Platform.gd
@@ -2,59 +2,40 @@ extends RigidBody2D
 
 @export_custom(PROPERTY_HINT_NONE,"suffix:s") var cargo_drop_time: float = 1.5
 
-@onready var _animator: AnimationPlayer = $AnimationPlayer
-@onready var _staticPlatform: Node2D = $StaticPlatform
-@onready var _animatedPlatform: Node2D = $AnimatedPlatform
-@onready var _staticPlatformCollider: CollisionShape2D = $StaticPlatformCollider
 @onready var _leftPlatformCollider: CollisionShape2D = $LeftPlatformCollider
 @onready var _rightPlatformCollider: CollisionShape2D = $RightPlatformCollider
+@onready var _leftSprite : Sprite2D = get_node("LeftPlatform")
+@onready var _rightSprite : Sprite2D = get_node("RightPlatform")
 @onready var _lplayer : Character = get_node("../LeftCharacter")
 @onready var _rplayer : Character = get_node("../RightCharacter")
 
-signal _dropRelease
+var _currentAngle : float = 0
+const MAX_ANGLE : float =  PI / 2.2
+const OPEN_SPEED : float = PI / 1.2
+const CLOSE_SPEED : float = -PI / 2.5
 
 func _physics_process(delta: float) -> void:
 	self.gravity_scale = 0.0 if _noPlayerInput() else 0.6
+	if Input.is_action_pressed("Drop"):
+		_rotateCargoDoors(OPEN_SPEED * delta)
+	elif _currentAngle > 0: #If we aren't trying to open, then try to close
+		_rotateCargoDoors(CLOSE_SPEED * delta)
+
+##keep the animation as close doors, if we want to drop, play in reverse until we want to resume...?
+
 func _noPlayerInput() -> bool:
 	return _lplayer.outputDir == Vector2.ZERO and _rplayer.outputDir == Vector2.ZERO
 
-var _dropping: bool = false
+func _rotateCargoDoors(angleDelta : float):
+	_currentAngle = clampf( _currentAngle + angleDelta, 0, MAX_ANGLE)
+	_setRotations(_currentAngle)
 
-func _input(event: InputEvent) -> void:
-	if event.is_action("Drop"):
-		_dropCargo()
-	if event.is_action_released("Drop"):
-		if _animator.is_playing():
-			await _animator.animation_finished
-		_dropRelease.emit()
+func _setRotations(angle : float):
+	_leftPlatformCollider.rotation = angle
+	_rightPlatformCollider.rotation = -angle
+	_leftSprite.rotation = angle
+	_rightSprite.rotation = -angle
 
-func _dropCargo() -> void:
-	if _dropping: return
-	_dropping = true
-	_open()
-	await _dropRelease
-	await _close()
-	_dropping = false
-
-	
-func _cargoDoors(open:bool) -> void:
-	var animationName = "openDoors" if open else "closeDoors"
-	_animator.play(animationName)
-	
-func _open() -> void:
-	_leftPlatformCollider.disabled = false
-	_rightPlatformCollider.disabled = false
-	_staticPlatformCollider.disabled = true
-	_animatedPlatform.show()
-	_staticPlatform.hide()
-	_cargoDoors(true)
-	
-func _close() -> void:
-	_cargoDoors(false)
-	await _animator.animation_finished
-	_staticPlatformCollider.disabled = false	
-	_leftPlatformCollider.disabled = true
-	_rightPlatformCollider.disabled = true
-	_staticPlatform.show()
-	_animatedPlatform.hide()
-	set_collision_layer_value(4, true)
+##Maybe apply a little help to crates in the middle so that they can get picked up more easily?
+func _applyCrateLift():
+	pass 
