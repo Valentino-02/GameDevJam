25f004cb165817fa8ba45c939b89f82e70d7891f
Tibor <tiborcmester@gmail.com>
2025-04-22 21:19:14 -0400

    changed rain clouds to use mask instead of collisions

--- src/scenes/Game/Components/RainObstacle/RainCloud.gd
+++ src/scenes/Game/Components/RainObstacle/RainCloud.gd
@@ -8,7 +8,9 @@ var _raining : bool = true
 signal rainingChanged
 
 @onready var _hitbox : Area2D = get_node("RainHitBox")
-@onready var _particlePlayer : CPUParticles2D = get_node("RainEmitterPlayer")
+@onready var _particlePlayer : CPUParticles2D = get_node("ParticleMask/RainEmitterPlayer")
+@onready var _raycast : RayCast2D = get_node("RayCast2D")
+@onready var _mask : Sprite2D = get_node("ParticleMask")
 @onready var _particleBack : CPUParticles2D = get_node("RainEmitterBehind")
 @onready var _audioPlayer : AudioStreamPlayer2D = AudioManager.sfx.createPlayer2D(ResourceIds.SfxId.Raining)
 
@@ -51,3 +53,14 @@ func _onRainEntered(body:Node2D) -> void:
 	if body is RigidBody2D and _raining:
 		body.physics_material_override = slippery_material
 		
+func _physics_process(delta: float) -> void:
+	_scaleMask(delta)
+
+func _scaleMask(delta : float):
+	var dist : float
+	if _raycast.is_colliding():
+		dist = abs((to_local(_raycast.get_collision_point()) - _raycast.position).y)
+	else:
+		dist = abs((_raycast.target_position - _raycast.position).y)
+	_mask.scale.y = lerp(_mask.scale.y, dist / 100.0, delta * 10)
+	_mask.position.y = _mask.scale.y * 50.0
--- src/scenes/Game/Components/Walls/WindObstacle.gd
+++ src/scenes/Game/Components/Walls/WindObstacle.gd
@@ -4,16 +4,10 @@ enum windDirection {
 	DOWN
 }
 
-##Apply this force along the line
-@export var force: float = 800
-@export var platformForceModifier: float = 6
 @export var forceDirection: windDirection
 @export var blastDuration: float = 6
 
-##Multiplier for the heavier platform
-var _platformForce: float:
-	get:
-		return force * platformForceModifier
+
 ##Current bodies within collider
 var _bodies: Array[RigidBody2D] = []
 var _direction: Vector2:
@@ -28,18 +22,12 @@ var _direction: Vector2:
 var _active: bool = false
 var activeParticles: GPUParticles2D
 
-@onready var _area: Area2D = $Area2D
 @onready var _bottomParticles: GPUParticles2D = $SpriteMask/BottomParticles
 @onready var _topParticles: GPUParticles2D = $SpriteMask/TopParticles
 @onready var _audioPlayer : AudioStreamPlayer2D =AudioManager.sfx.createPlayer2D(ResourceIds.SfxId.Wind)
 
 
 func _ready() -> void:
-	_area.body_entered.connect(_addBody)
-	###----This is only as a temporary activate-on-enter----
-	_area.body_entered.connect(Activate)
-	###---------
-	_area.body_exited.connect(_removeBody)
 	activeParticles = _bottomParticles if forceDirection == windDirection.UP else _topParticles
 	activeParticles.show()
 
@@ -49,7 +37,6 @@ func _ready() -> void:
 ##This should be used to activate the wind tunnel
 func Activate(_node: Node2D = null) -> void:
 	if _active: return
-	if _bodies.size() <= 0: return
 	_changeParticleSpeed(500)
 	_changeParticleQuantity(12)
 	_audioPlayer.play(0.0)
@@ -61,22 +48,7 @@ func Activate(_node: Node2D = null) -> void:
 	_audioPlayer.stop()
 	_changeParticleSpeed(80)
 	_changeParticleQuantity(8)
-	
-func _addBody(body: Node2D) -> void:
-	var groups: Array[StringName] = body.get_groups()
-	if groups.has("Player") || groups.has("PlayerPlatform"):
-		_bodies.append(body)
-
-func _removeBody(body: Node2D) -> void:
-	var groups: Array[StringName] = body.get_groups()
-	if (groups.has("Player") || groups.has("PlayerPlatform")) && _bodies.has(body):
-		_bodies.erase(body)
 		
-func _physics_process(_delta: float) -> void:
-	if !_active: return
-	for body in _bodies:
-		var selectedForce = force if body.get_groups().has("Player") else _platformForce
-		#body.apply_force(_direction * selectedForce, activeParticles.position)
 
 func _changeParticleSpeed(speed: float) -> void:
 	var targetVelocity = activeParticles.process_material.get("initial_velocity")
