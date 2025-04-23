417e6a68de7717e3b8ee5980c9191a61ab0308a9
tibormester <67936278+tibormester@users.noreply.github.com>
2025-04-23 10:00:51 -0400

    bugfix (#67)

--- src/scenes/Game/PlayerCharacter/Rope.gd
+++ src/scenes/Game/PlayerCharacter/Rope.gd
@@ -9,20 +9,21 @@ class_name Rope extends Line2D
 @export var platform: RigidBody2D
 @export var platform_attachement : Node2D
 
-@onready var particles: RopeParticles = get_node("ParticleMask/GPUParticles2D")
+@onready var _mask : Sprite2D = get_node("ParticleMask")
+@onready var particles: CPUParticles2D = get_node("ParticleMask/GPUParticles2D")
 
 func _ready() -> void:
-	particles.SetMaskAndRope(get_node("ParticleMask"), self)
+	particles.emitting = true
 
 func _physics_process(_delta):
-	var a_pos = character.global_position
-	var b_pos = platform_attachement.global_position
-
 	# Update line points
-	points = [to_local(a_pos), to_local(b_pos)]
+	points = [to_local(character.global_position), to_local(platform_attachement.global_position)]
+	_updateSpringForce()
+	_updateRopeParticles()
 
+func _updateSpringForce():
 	# Spring vector
-	var diff = b_pos - a_pos
+	var diff = platform_attachement.global_position - character.global_position
 	var currentLength = diff.length()
 	var direction = diff.normalized()
 
@@ -43,6 +44,10 @@ func _physics_process(_delta):
 
 	platform.apply_force(platform_force, platform_attachement.global_position - platform.global_position)
 
+func _updateRopeParticles():
+	_mask.position = self.points[0]
+	_mask.rotation = self.points[0].direction_to(self.points[1]).angle()
+	_mask.texture.size.x = self.points[0].distance_to(self.points[1])
 
 func _calculateCriticalDampening(mass : float, spring_strength : float) -> float:
 	return 2 * sqrt(spring_strength * mass)
