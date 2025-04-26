1f392c136c2f287140f30dac719c761c24af7202
Tibor <67936278+tibormester@users.noreply.github.com>
2025-04-25 22:01:55 -0400

    Obstacle fixes (#99)
    * fixed fireball and wind obstacle feelings
    
    * made platform more responsive and fixed raincloud bug
    
    * idrk

--- src/scenes/Game/PlayerCharacter/Character.gd
+++ src/scenes/Game/PlayerCharacter/Character.gd
@@ -10,10 +10,11 @@ class_name Character extends Area2D
 @export var _radius : float = 50
 @export var _speed : float = 200
 @export var returnSpeed : float = 200
-var outputDir : Vector2
-const STRUCK_TIME : int = 2500
+var outputDir : Vector2 = Vector2.ZERO
+const STRUCK_TIME : int = 100
 var _struck : int = Time.get_ticks_msec() - STRUCK_TIME
-
+var _struckDir : Vector2 = Vector2.ZERO
+const STRUCK_SPEED : float = 150
 @onready var _origin : Marker2D = get_node("../"+_suffix + "Origin")
 
 func _ready() -> void:
@@ -24,8 +25,16 @@ func _ready() -> void:
 	SignalBus.zonePatienceEnded.connect(_onZonePatienceEnded)
 	SignalBus.bothScoreMaxed.connect(_onBothScoreMaxed)
 
+var _previousValidPosition
 #get the target position = back to origin or current position + speed in input direciton
 func _physics_process(delta: float) -> void:
+	if _outsideBoundary():
+		global_position = _previousValidPosition
+		outputDir = Vector2.ZERO
+		return
+	else:
+		_previousValidPosition = global_position
+
 	var inputDir : Vector2 = _getInputVector()
 			
 	_origin.global_position = _rope.platform_attachement.global_position + Vector2.UP * _rope.springLength
@@ -34,9 +43,10 @@ func _physics_process(delta: float) -> void:
 	#if in an up wind tunnel:
 	if _inWind():
 		origin += Vector2.UP * _radius * 0.55
-	#If we were struck by a fireball or in the rain, register it as a constant downwards input
-	if (Time.get_ticks_msec() < _struck + STRUCK_TIME) or _inRainCloud(): 
+	if _inRainCloud(): 
 		origin += Vector2.DOWN * _radius * 0.55
+	
+	
 
 	#move towards the joystick's origin if no input, otherwise go with the input
 	if inputDir == Vector2.ZERO:
@@ -50,6 +60,11 @@ func _physics_process(delta: float) -> void:
 	
 	outputDir = global_position - _origin.global_position
 
+	#If we were struck by a fireball push us in that direction
+	if (Time.get_ticks_msec() < _struck + STRUCK_TIME):
+		global_position += _struckDir * STRUCK_SPEED * delta 
+
+	
 
 	
 func _getInputVector() -> Vector2:
@@ -84,15 +99,26 @@ func _inWind() -> bool:
 func _collideArea(area):
 	if area is Fireball:
 		if element == Types.Element.Fire:
+			area.explosion_strength *= 0.5
+			area._shape_cast.shape.radius *= 0.5
 			area._onCollision(self)
 		else:
 			area.explosion_strength *= 2
 			area._shape_cast.shape.radius *= 2
 			area._onCollision(self)
-			_struck = Time.get_ticks_msec()
+			
+
+func _onFireball(direction : Vector2):
+	_struck = Time.get_ticks_msec()
+	_struckDir = direction
+
+		#need to do something here to ensure we don't leave the boundary, but implementing move and slide is so tedious
 
 func _onZonePatienceEnded(_zone : Types.Zone) -> void:
 	set_physics_process(false)
 
 func _onBothScoreMaxed() -> void:
 	set_physics_process(false)
+
+func _outsideBoundary() -> bool:
+	return get_overlapping_bodies().any(func (x): return x.get_parent() is BoundaryWall)
