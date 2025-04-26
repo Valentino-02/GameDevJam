f66a1aea060c6e74f2868aba1c94b6fb6677dfc4
Tibor <tiborcmester@gmail.com>
2025-04-26 02:07:22 -0400

    i broke the wind particles when i gutted the tool code cause i couldnt understand it, this is a patchwork fix

--- src/scenes/Game/Components/Walls/WindObstacle.gd
+++ src/scenes/Game/Components/Walls/WindObstacle.gd
@@ -28,17 +28,17 @@ var _direction: Vector2:
 			_:
 				return Vector2.ZERO
 var _active: bool = false
-var activeParticles: GPUParticles2D
+var activeParticles: CPUParticles2D
 
 
-@onready var _bottomParticles: GPUParticles2D = $SpriteMask/BottomParticles
-@onready var _topParticles: GPUParticles2D = $SpriteMask/TopParticles
+@onready var _bottomParticles: CPUParticles2D = $SpriteMask/BottomParticles
+@onready var _topParticles: CPUParticles2D = $SpriteMask/TopParticles
 var _audioPlayer: AudioStreamPlayer2D 
 @onready var _cooldownTimer: Timer = %CooldownTimer
 
 func _ready() -> void:
 	if Engine.is_editor_hint(): return
-	_updateArea()
+	#_updateArea()
 	_audioPlayer = AudioManager.sfx.createPlayer2D(ResourceIds.SfxId.Wind)
 	activeParticles = _bottomParticles if forceDirection == windDirection.UP else _topParticles
 	activeParticles.show()
@@ -84,9 +84,9 @@ func _physics_process(delta: float) -> void:
 				body.apply_central_force(_direction * FORCE_STRENGTH)
 
 func _changeParticleSpeed(speed: float) -> void:
-	var targetVelocity = activeParticles.process_material.get("initial_velocity")
-	targetVelocity.y = speed
-	activeParticles.process_material.set("initial_velocity", targetVelocity)
+	activeParticles.initial_velocity_min = speed
+	activeParticles.initial_velocity_max = speed * 1.2
+
 
 func _changeParticleQuantity(quantity: int) -> void:
 	activeParticles.amount = quantity
@@ -101,8 +101,8 @@ func _updateArea() -> void:
 	texture.region = shape.get_rect()
 	var height: float = shape.get_rect().size.y / 2
 	var width: float = shape.get_rect().size.x / 2
-	_bottomParticles.position = Vector2(0, height)
-	_topParticles.position = Vector2(0, -height)
+	_bottomParticles.position = Vector2(0, 0)
+	_topParticles.position = Vector2(0, -0)
 	var boxExtents: Vector3 = Vector3(width, height, 0)
 	_bottomParticles.process_material.set("emission_box_extents", boxExtents)
 	_topParticles.process_material.set("emission_box_extents", boxExtents)
